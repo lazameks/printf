804abeae07d6018e2544f401b37c93032b95b52f
Anibuali Emeka <lazameks@gmail.com>
2023-08-21 22:37:19 +0000

    edge case

--- _printf.c
+++ _printf.c
@@ -17,9 +17,10 @@ int _printf(const char *format, ...)
 		return (-1);
 	if (format[0] == '%' && format[1] == '\0')
 		return (-1);
-
+	if (format[0] == '%' && format[1] == 32)
+		return (-1);
 	va_start(args, format);
-	
+
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
