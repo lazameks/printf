f34854cca6c907575207d52253f2c65afcbe35e1
kwakye02 <kwakyegeorge0@gmail.com>
2023-08-22 18:09:43 -0700

    updating printf.c file

--- _printf.c
+++ _printf.c
@@ -1,55 +1,56 @@
 #include "main.h"
-#include <stddef.h>
-
 /**
- * _printf - print endless args
- * @format: the first char pointer
- *
- * Return: Number of characters printed
- */
-
+*_printf - prouduces  an output according to the format given to it.
+*@format: The character string to be printed
+*Return: Returns number of characters printed or (null ) or -1
+*/
 int _printf(const char *format, ...)
 {
-	int i, r_val = 0;
-	char *str;
+	int char_count = 0, string_length;
 	va_list args;
+	char *str, character;
 
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 	if (format[0] == '%' && format[1] == 32)
 		return (-1);
 	va_start(args, format);
-
-	for (i = 0; format[i] != '\0'; i++)
+	while (*format)
 	{
-		if (format[i] != '%')
+		if (*format != '%')
 		{
-			_putchar(format[i]);
-		}
-		else if (format[i + 1] == 'c')
-		{
-			_putchar((char)(va_arg(args, int)));
-			i++;
-		}
-		else if (format[i + 1] == 's')
-		{
-			str = va_arg(args, char *);
-			if (str == NULL)
-				str = "(null)";
-			r_val += _putstr(str);
-			r_val--;
-			i++;
-		}
-		else if (format[i + 1] == '%')
-		{
-			_putchar('%');
-			i++;
+			char_count++;
+			write(1, format, 1);
 		}
 		else
-			return (-1);
-		r_val += 1;
+		{
+			format++;
+			if (*format == 'c')
+			{
+				character = (char)va_arg(args, int);
+				write(1, &character, 1);
+				char_count++;
+
+			}
+			else if (*format == 's')
+			{
+				str = va_arg(args, char*);
+				if (str == NULL)
+					str = "(null)";
+				string_length = strlen(str);
+				write(1, str, string_length);
+				char_count += string_length;
+			}
+			else if (*format == '%')
+			{
+				write(1, format, 1);
+				char_count++;
+			}
+		}
+		format++;
+
 	}
 	va_end(args);
+	return (char_count);
 
-	return (r_val);
 }
