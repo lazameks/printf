ae7b56084e8994a8dce619abb0c2e0baed7d4079
kwakye02 <kwakyegeorge0@gmail.com>
2023-08-22 18:19:43 -0700

    updated files

--- main.h
+++ main.h
@@ -1,13 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 /* Standard C library */
+#include <stddef.h>
 #include <unistd.h>
 #include <stdarg.h>
 #include <limits.h>
+#include <string.h>
 
 /* Functions Header prototypes*/
 int _printf(const char *format, ...);
 int _putchar(char c);
 int _putstr(char *s);
-
+int _str_len(char *str);  
 #endif
